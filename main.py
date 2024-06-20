0efdfd58a90ae5493f62332aefef6182ceeef929
rpfozzy <fozifozzy@gmail.com>
2024-06-20 14:17:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import telebot
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from pytube import YouTube
+from moviepy.editor import AudioFileClip
+import os
+import requests
 
 # Вставьте ваш токен сюда
 token = '6884297621:AAGduHkNhhs08N--1B1rsh4rsMycmjocuXo'
 
 # Создайте экземпляр TeleBot
 bot = telebot.TeleBot(token)
+
+# Код доступа для скачивания видео
+ACCESS_CODE = '2007'
+user_state = {}
 
 # Функция для обработки команды /start
 @bot.message_handler(commands=['start'])
@@ -21,5 +29,26 @@ def start(message):
     
     bot.send_message(message.chat.id, text, reply_markup=keyboard, parse_mode='MarkdownV2')
 
-# Запуск бота
-bot.polling()
+# Функция для обработки команды /y
+@bot.message_handler(commands=['y'])
+def request_code(message):
+    bot.send_message(message.chat.id, "Введите код:")
+    user_state[message.chat.id] = 'WAITING_FOR_CODE'
+
+# Функция для обработки текстовых сообщений
+@bot.message_handler(func=lambda message: True)
+def handle_text(message):
+    if message.chat.id in user_state:
+        state = user_state[message.chat.id]
+        if state == 'WAITING_FOR_CODE':
+            if message.text == ACCESS_CODE:
+                bot.send_message(message.chat.id, "Код принят! Введите ссылку на видео с YouTube:")
+                user_state[message.chat.id] = 'WAITING_FOR_LINK'
+            else:
+                bot.send_message(message.chat.id, "Неверный код. Попробуйте снова.")
+        elif state == 'WAITING_FOR_LINK':
+            link = message.text
+            try:
+                yt = YouTube(link)
+                video_title = yt.title
+                duration = yt.length
